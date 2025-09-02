3be785a85f5c9621fc5a54336a5ef8f960c075df
DEV <divyanshu983770@gmail.com>
2025-09-02 12:04:01 +0530

    first commit

---
+++ server/db/db.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const connectToMongoDB = async () =>{
+    try{
+        await mongoose.connect("mongodb://localhost:27017/note_app");
+        console.log("Connect to MongoDB")
+    }catch(err){
+        console.log("Error connecting to MongoDB", err.message)
+    }
+}
+
+
+export default connectToMongoDB;
